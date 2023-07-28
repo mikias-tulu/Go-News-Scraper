5325dc15af589eb421924ad5268dbd88cef52842
mikias-tulu <miki.tulu@gmail.com>
2023-07-29 00:00:02 +0300

    Sending HTTP requests with a proxy

--- src/reviews.go
+++ src/reviews.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-   "encoding/csv"
-   "fmt"
-   "log"
-   "os"
+	//"encoding/csv"
+	"fmt"
+	//"log"
+	//"os"
 
-   "github.com/gocolly/colly"
+	"github.com/gocolly/colly"
 )
+
 func main() {
-   // Scraping code here
-   fmt.Println("Done")
+	c := colly.NewCollector(colly.AllowedDomains("www.amazon.in"))
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Link of the page:", r.URL)
+	})
+
+	c.Visit("https://www.amazon.in/s?k=keyboard")
 }
